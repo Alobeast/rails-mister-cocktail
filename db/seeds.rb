4c4bfbdee5b35832650023ed7b03ef935b7428ef
Changhun Yun <isaac.c.yun@gmail.com>
2019-02-22 11:59:53 +0100

    push cocktail master front end day

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Your ingredients are being created.."
+Ingredient.create(name: 'lemon')
+Ingredient.create(name: 'Coke')
+Ingredient.create(name: 'Sprite')
+Ingredient.create(name: 'Orange')
+Ingredient.create(name: 'Tomato juice')
+Ingredient.create(name: 'ice')
+Ingredient.create(name: 'mint leaves')
+puts "Your ingredients are created!"
